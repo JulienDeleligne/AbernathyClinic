c4fddc14a3973e486c5eb0f85b16746a003a27c0
Julien Deleligne <julien.deleligne@decathlon.com>
2021-04-26 11:13:37 +0200

    first commit

---
+++ client-ui/src/test/java/com/AbernathyClinic/clientui/ClientUiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AbernathyClinic.clientui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
